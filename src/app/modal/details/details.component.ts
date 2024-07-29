677a48bb2a20e88fce0c32741a4d12c267ec6506
leomiclos <leonardor869@gmail.com>
2024-07-29 08:19:31 -0300

    feat: adicionado loading ao gerar pdf

--- src/app/modal/details/details.component.ts
+++ src/app/modal/details/details.component.ts
@@ -37,42 +37,93 @@ export class DetailsComponent {
     const part3 = document.getElementById('contentToConvertPart3');
 
     if (part1 && part2 && part3) {
-        html2canvas(part1, { scale: 2, scrollY: -window.scrollY }).then((canvas1) => {
-            html2canvas(part2, { scale: 2, scrollY: -window.scrollY }).then((canvas2) => {
-                const pdf = new jsPDF('p', 'mm', 'a4');
-                const pageWidth = pdf.internal.pageSize.getWidth();
-                const pageHeight = pdf.internal.pageSize.getHeight();
+      // Mostrar o loading
+      const loadingElement = document.getElementById('loading');
+      const buttonDownload = document.getElementById('button')
+      if (loadingElement && buttonDownload) {
+        buttonDownload.style.display = 'none'
+        loadingElement.style.display = 'block';
+      }
 
-                const imgWidth1 = canvas1.width / 2;
-                const imgHeight1 = canvas1.height / 2;
-                const imgWidth2 = canvas2.width / 2;
-                const imgHeight2 = canvas2.height / 2;
+      html2canvas(part1, { scale: 2, scrollY: -window.scrollY }).then(
+        (canvas1) => {
+          html2canvas(part2, { scale: 2, scrollY: -window.scrollY }).then(
+            (canvas2) => {
+              const pdf = new jsPDF('p', 'mm', 'a4');
+              const pageWidth = pdf.internal.pageSize.getWidth();
+              const pageHeight = pdf.internal.pageSize.getHeight();
 
-                const ratio1 = Math.min(pageWidth / imgWidth1, pageHeight / imgHeight1);
-                const ratio2 = Math.min(pageWidth / imgWidth2, pageHeight / imgHeight2);
+              const imgWidth1 = canvas1.width / 2;
+              const imgHeight1 = canvas1.height / 2;
+              const imgWidth2 = canvas2.width / 2;
+              const imgHeight2 = canvas2.height / 2;
 
-                // Adiciona a primeira parte na posição inicial (0, 0)
-                pdf.addImage(canvas1.toDataURL('image/png', 1.0), 'PNG', 0, 0, imgWidth1 * ratio1, imgHeight1 * ratio1);
+              const ratio1 = Math.min(
+                pageWidth / imgWidth1,
+                pageHeight / imgHeight1
+              );
+              const ratio2 = Math.min(
+                pageWidth / imgWidth2,
+                pageHeight / imgHeight2
+              );
 
-                // Adiciona a segunda parte logo abaixo da primeira
-                const secondPartY = imgHeight1 * ratio1 + 10; // 10 mm de espaço entre as partes
-                pdf.addImage(canvas2.toDataURL('image/png', 1.0), 'PNG', 0, secondPartY, imgWidth2 * ratio2, imgHeight2 * ratio2);
+              // Adiciona a primeira parte na posição inicial (0, 0)
+              pdf.addImage(
+                canvas1.toDataURL('image/png', 1.0),
+                'PNG',
+                0,
+                0,
+                imgWidth1 * ratio1,
+                imgHeight1 * ratio1
+              );
 
-                // Captura a terceira parte e adiciona na segunda página
-                html2canvas(part3, { scale: 2, scrollY: -window.scrollY }).then((canvas3) => {
-                    const imgWidth3 = canvas3.width / 2;
-                    const imgHeight3 = canvas3.height / 2;
-                    const ratio3 = Math.min(pageWidth / imgWidth3, pageHeight / imgHeight3);
+              // Adiciona a segunda parte logo abaixo da primeira
+              const secondPartY = imgHeight1 * ratio1 + 10; // 10 mm de espaço entre as partes
+              pdf.addImage(
+                canvas2.toDataURL('image/png', 1.0),
+                'PNG',
+                0,
+                secondPartY,
+                imgWidth2 * ratio2,
+                imgHeight2 * ratio2
+              );
 
-                    pdf.addPage();
-                    pdf.addImage(canvas3.toDataURL('image/png', 1.0), 'PNG', 0, 0, imgWidth3 * ratio3, imgHeight3 * ratio3);
+              // Captura a terceira parte e adiciona na segunda página
+              html2canvas(part3, { scale: 2, scrollY: -window.scrollY }).then(
+                (canvas3) => {
+                  const imgWidth3 = canvas3.width / 2;
+                  const imgHeight3 = canvas3.height / 2;
+                  const ratio3 = Math.min(
+                    pageWidth / imgWidth3,
+                    pageHeight / imgHeight3
+                  );
 
-                    pdf.save('relatorio-de-visitas.pdf');
-                });
-            });
-        });
+                  pdf.addPage();
+                  pdf.addImage(
+                    canvas3.toDataURL('image/png', 1.0),
+                    'PNG',
+                    0,
+                    0,
+                    imgWidth3 * ratio3,
+                    imgHeight3 * ratio3
+                  );
+
+                  // Esconder o loading
+
+
+                  pdf.save('relatorio-de-visitas.pdf');
+                }
+              );
+
+              if(loadingElement && buttonDownload){
+                loadingElement.style.display = 'none';
+                buttonDownload.style.display = 'block'
+
+              }
+            }
+          );
+        }
+      );
     }
-}
-
-
+  }
 }
